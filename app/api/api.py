c3a51acda69f6297ead67aacb00758c2922c5709
Nik Krumm <nkrumm@gmail.com>
2019-09-10 00:09:14 -0700

    First pass at workflow launch API and logs API

--- app/api/api.py
+++ app/api/api.py
@@ -1,26 +1,126 @@
-
+import boto3
+import uuid
+from flask import current_app
 from flask.views import MethodView
 from flask_rest_api import Blueprint, abort
+from marshmallow import Schema, INCLUDE, EXCLUDE, fields
+
+ecs_client = boto3.client('ecs')
+s3_client = boto3.client('s3')
+logs_client = boto3.client('logs')
+
+class CreateWorkflowArgs(Schema):
+    # The nextflow_workflow and nextflow_config fields
+    # are a bit of a hack so that form data and file data can 
+    # be sent via the same POST.
+    # Future versions of `flask_rest_api` may more natively support this.
+    class Meta:
+        unknown = INCLUDE    
+    nextflow_version = fields.String(location="form")
+    nextflow_arguments = fields.String(location="form")
+    nextflow_workflow = fields.Function(location="files", deserialize=lambda x: x.read().decode("utf-8"))
+    nextflow_config = fields.Function(location="files", deserialize=lambda x: x.read().decode("utf-8"))
+
+class CreateWorkflowReturn(Schema):
+    class Meta:
+        unknown = EXCLUDE
+    taskArn = fields.String()
+    taskLastStatus = fields.String()
+    containerLastStatus = fields.String()
 
 WorkflowApi = Blueprint(
     'WorkflowApi', __name__,
     description='Create and monitor Nextflow workflows.'
 )
 
-
 @WorkflowApi.route('/workflow')
 class ListWorkflows(MethodView):
+    def _generate_key(self):
+        return str(uuid.uuid4())
+
+    def _upload_to_s3(self, s3_key, contents):
+        res = s3_client.put_object(
+            Body=contents,
+            Bucket=current_app.config["NEXTFLOW_S3_TEMP"],
+            Key=s3_key
+        )
+
     def get(self):
         """List all workflows"""
         return {"msg": "hello world."}
 
-    def post(self):
+    @WorkflowApi.arguments(CreateWorkflowArgs)
+    @WorkflowApi.response(code=201)
+    def post(self, args):
         """Submit new workflow for execution"""
-        return {"msg": "hello world."}        
+        
+        # 1. If a workflow and config file was uploaded
+        if ("nextflow_workflow" in args) and ("nextflow_config" in args):
+            uuid_key = self._generate_key()
+            workflow_key = "nextflow_scripts/%s/main.nf" % uuid_key
+            config_key = "nextflow_scripts/%s/nextflow.config" % uuid_key
+            self._upload_to_s3(workflow_key, args["nextflow_workflow"])
+            self._upload_to_s3(config_key, args["nextflow_config"])
+        # elif
+        # -- GIT link provided. Probably pass to nf directly? 
+        # elif
+        # -- URL link; probably download and upload to s3 as above
+        else:
+            return "Invalid nextflow commands", 500
+        
+        print(workflow_key, config_key)
+        workflow_s3_loc = "s3://%s/%s" % (current_app.config["NEXTFLOW_S3_TEMP"], workflow_key)
+        config_s3_loc = "s3://%s/%s" % (current_app.config["NEXTFLOW_S3_TEMP"], config_key)
+
+        res = ecs_client.run_task(
+            cluster=current_app.config["ECS_CLUSTER"],
+            taskDefinition=current_app.config["NEXTFLOW_TASK_DEFINITION"],
+            overrides={
+                "containerOverrides": [
+                    {
+                        "name": "nextflow",
+                        "command": ["runner.sh", workflow_s3_loc, config_s3_loc]
+                    }
+                ]
+            },
+            launchType="FARGATE",
+            networkConfiguration={
+                "awsvpcConfiguration": {
+                    "subnets": current_app.config["ECS_SUBNETS"],
+                    "assignPublicIp": "ENABLED"
+                },
+            },
+        )
+
+        taskArn = res['tasks'][0]['taskArn']
+        taskLastStatus = res['tasks'][0]['lastStatus']
+        containerLastStatus = res['tasks'][0]['containers'][0]['lastStatus']
+
+        return {
+            "taskArn": taskArn,
+            "taskLastStatus": taskLastStatus,
+            "containerLastStatus": containerLastStatus,
+        }
 
 @WorkflowApi.route('/workflow/<string:id>')
 class ListWorkflows(MethodView):
     def get(self, id ):
         """Get information on workflow by workflow id"""
-        return {"msg": "hello %s" % id}
+        res = ecs_client.describe_tasks(
+            cluster=current_app.config["ECS_CLUSTER"],
+            tasks=[id]
+        )
+        return res
+
+
+@WorkflowApi.route('/workflow/<string:id>/logs')
+class WorkflowLogs(MethodView):
+    def get(self, id ):
+        """Get top level workflow logs"""
+        res = logs_client.get_log_events(
+            logGroupName='/ecs/nextflow-runner',
+            logStreamName="ecs/nextflow/%s" % id,
+            startFromHead=False
+        )
+        return res
 
--- app/config.py
+++ app/config.py
@@ -9,10 +9,18 @@ class Config(object):
     OPENAPI_VERSION = '3.0.2'
     API_PREFIX = '/api/v1'
 
+    # ECS/Batch configuration
+    ECS_CLUSTER = 'aws-batch-ce_Batch_1031105a-6a87-3673-8196-1d5723fb2991'
+    ECS_SUBNETS = ["subnet-08f6b618bb7438e46"]
+    NEXTFLOW_TASK_DEFINITION = 'nextflow-runner:10'
+    NEXTFLOW_S3_TEMP = 'ncgl-dev-nkrumm.home-bucket'
+
+
 class ProductionConfig(Config):
     AUTH_METHOD = 'SAML'
 
 class DevelopmentConfig(Config):
     AUTH_METHOD = 'MOCK'
-    OPENAPI_URL_PREFIX = 'docs'
-    OPENAPI_REDOC_PATH = 'redoc'
+    OPENAPI_URL_PREFIX = '/docs'
+    OPENAPI_REDOC_PATH = '/redoc'
+    OPENAPI_SWAGGER_UI_PATH = '/swagger'
